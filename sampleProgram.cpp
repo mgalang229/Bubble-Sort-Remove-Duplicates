103f8f3185deb2516b1c39d74a19d07507f0490b
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2019-12-01 12:00:19 +0800

    Add files via upload

--- sampleProgram.cpp
+++ sampleProgram.cpp
@@ -3,44 +3,39 @@ using namespace std;
 
 int main(){
 	
-	int n, arr[100], temp[100], TEMP;
+	int n, temp, arr[100], arr2[100];
 	
-	cout << "Enter n: ";
 	cin >> n;
-	
-	cout << "Enter elements of array: " << endl;
-	for(int i = 0; i < n; i++){
+	for(int i = 1; i <= n; i++){
 		cin >> arr[i];
 	}
 	
-	for(int i = 0; i < n; i++){
-		for(int j = i + 1; j <= n; j++){
-			if(arr[i] > arr[j]){
-				TEMP = arr[i];
-				arr[i] = arr[j];
-				arr[j] = TEMP;
+	for(int i = 1; i < n; i++){
+		for(int j = 1; j <= n-1; j++){
+			if(arr[j] > arr[j+1]){
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;	
 			}
 		}
 	}
-	cout << "\nBUBBLE SORT: " << endl;
-	for(int i = 0;i < n; i++){
+	
+	for(int i = 1; i <= n; i++){
 		cout << arr[i] << " ";
 	}
 	cout << endl;
 	
-	int j = 0;
-	for(int i = 0; i < n-1; i++){
+	int j = 1;
+	for(int i = 1; i <= n; i++){
 		if(arr[i] != arr[i+1]){
-			temp[j] = arr[i];
+			arr2[j] = arr[i];
 			j++;
 		}
 	}
-	cout << "\nLIST WITHOUT DUPLICATES: " << endl;
-	temp[j] = arr[n-1];
-	for(int i = 0; i < j; i++){
-		cout << temp[i] << " ";
+
+	for(int i = 1; i < j; i++){
+		cout << arr2[i] << " ";
 	}
-	cout << temp[j] << endl;
 	
 	return 0;
 }
